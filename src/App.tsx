2ca982503f4109f3938eac372ee648b081ea1a73
devfle <hello@flesch-design.de>
2021-10-23 13:38:22 +0200

    feat(react-router): add react router to application

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 
 // Custom Components
@@ -6,9 +7,23 @@ import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
-    <div className="quote-manager">
-      <Navigation />
-    </div>
+    <Router>
+      <div className="quote-manager">
+        <Navigation />
+        <h1 className="quote-manager-title">Dashboard</h1>
+        <Switch>
+          <Route exact path="/">
+            <h1>Esel</h1>
+          </Route>
+          <Route path="/Quotes">
+            <h1>Quotes</h1>
+          </Route>
+          <Route path="*">
+            <h1>Seite nicht gefunden.</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import "./Navigation.scss";
+import { Link } from "react-router-dom";
 
 import Avatar from "@mui/material/Avatar";
 
 function Navigation() {
+  const navigationPointList: string[] = ["Dashboard", "Quotes", "Settings"];
+  const navigationPoints = navigationPointList.map((navigationString) => (
+    <li key={navigationString} className="qm-navigation__list-item">
+      <Link to={`/${navigationString}`}>{navigationString}</Link>
+    </li>
+  ));
   return (
     <nav className="qm-navigation">
-      <Avatar alt="User Avatar" sx={{ width: 72, height: 72 }} className="qm-navigation__avatar">
+      <Avatar alt="User Avatar" sx={{ width: 90, height: 90 }} className="qm-navigation__avatar">
         DF
       </Avatar>
+      <ul className="qm-navigation__list">{navigationPoints}</ul>
     </nav>
   );
 }
